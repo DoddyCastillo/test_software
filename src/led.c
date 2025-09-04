2cbe21ef3af39c4336ba0a5768ba7dcc8209ce99
DoddyCastillo <djcastil@espol.edu.ec>
2025-09-04 17:38:11 -0500

    Commit inical

---
+++ src/led.c
@@ -0,0 +1,6 @@
+#include "led.h"
+
+void LedsInitDriver(uint16_t *puerto_inicial)
+{
+    *puerto_inicial = 0x0000;
+}
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "../inc/suma.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int suma = 0;
+
+    suma += 5;
+    acumular(&suma, 3);
+    printf("Suma: %d\n", suma);
+
+    return 0;
+}
