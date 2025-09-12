b45c73de363f4b3b3116e67b456fc5a3c9179dbb
DoddyCastillo <djcastil@espol.edu.ec>
2025-09-11 22:07:41 -0500

    Test agregados y configuración de clang format y pre-commit

--- inc/led.h
+++ inc/led.h
@@ -2,3 +2,6 @@
 
 void LedsInitDriver(uint16_t *puerto_inicial);
 void LedsTurnOn(int led_number);
+void LedsTurnOff(int led_number);
+void LedsTurnAllOn(void);
+void LedsTurnAllOff(void);
--- src/led.c
+++ src/led.c
@@ -1,15 +1,38 @@
 #include "led.h"
 
+#define ALL_LEDS_OFF 0x0000;
+#define FIRST_BIT 1
+#define LED_TO_OFFSET 1
+
 static uint16_t *puerto;
+
+static uint16_t LedToMask(int led_number)
+{
+    return FIRST_BIT << (led_number - LED_TO_OFFSET);
+}
 
 void LedsInitDriver(uint16_t *puerto_inicial)
 {
     puerto = puerto_inicial;
-    *puerto_inicial = 0x0000;
+    LedsTurnAllOff();
 }
 
 void LedsTurnOn(int led_number)
 {
+    *puerto |= LedToMask(led_number);
+}
 
-    *puerto = 1 << 2;
+void LedsTurnOff(int led_number)
+{
+    *puerto &= ~LedToMask(led_number);
+}
+
+void LedsTurnAllOn(void)
+{
+    *puerto = ~ALL_LEDS_OFF;
+}
+
+void LedsTurnAllOff(void)
+{
+    *puerto = ALL_LEDS_OFF;
 }
