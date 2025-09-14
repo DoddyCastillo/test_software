33fef0d67978d06fc6ccbce575e760a67ab9f369
DoddyCastillo <djcastil@espol.edu.ec>
2025-09-14 15:15:49 -0500

    Pruebas completadas

--- inc/led.h
+++ inc/led.h
@@ -1,7 +1,9 @@
 #include <stdint.h>
+#include <stdbool.h>
 
 void LedsInitDriver(uint16_t * puerto_inicial);
 void LedsTurnOn(int led_number);
 void LedsTurnOff(int led_number);
 void LedsTurnAllOn(void);
 void LedsTurnAllOff(void);
+bool LedIsOn(int led_number);
--- src/led.c
+++ src/led.c
@@ -1,6 +1,8 @@
 #include "led.h"
+#include "errores.h"
+#include <stdbool.h>
 
-#define ALL_LEDS_OFF  0x0000;
+#define ALL_LEDS_OFF  0x0000
 #define FIRST_BIT     1
 #define LED_TO_OFFSET 1
 
@@ -10,16 +12,30 @@ static uint16_t LedToMask(int led_number) {
     return FIRST_BIT << (led_number - LED_TO_OFFSET);
 }
 
+static bool IsLedValid(int led) {
+    bool result = led >= 1 && led <= 16;
+    if (!result) {
+        Alerta("El led no es valido");
+    }
+    return result;
+}
+
 void LedsInitDriver(uint16_t * puerto_inicial) {
     puerto = puerto_inicial;
     LedsTurnAllOff();
 }
 
 void LedsTurnOn(int led_number) {
+    if (!IsLedValid(led_number)) {
+        return;
+    }
     *puerto |= LedToMask(led_number);
 }
 
 void LedsTurnOff(int led_number) {
+    if (!IsLedValid(led_number)) {
+        return;
+    }
     *puerto &= ~LedToMask(led_number);
 }
 
@@ -29,4 +45,11 @@ void LedsTurnAllOn(void) {
 
 void LedsTurnAllOff(void) {
     *puerto = ALL_LEDS_OFF;
+}
+
+bool LedIsOn(int led_number) {
+    if (!IsLedValid(led_number)) {
+        return false;
+    }
+    return (*puerto & LedToMask(led_number)) != 0;
 }
